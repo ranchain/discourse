c5291ed87fb15f6922b8d7afb5255333608dc7b4
Gerhard Schlager <mail@gerhard-schlager.at>
2018-03-20 18:02:52 +0100

    Add shared base class for TableDropper and ColumnDropper

--- lib/migration/column_dropper.rb
+++ lib/migration/column_dropper.rb
@@ -1,59 +1,16 @@
+require_dependency 'migration/base_dropper'
+
 module Migration
-  class ColumnDropper
+  class ColumnDropper < BaseDropper
     def self.drop(table:, after_migration:, columns:, delay: nil, on_drop: nil)
-      raise ArgumentError.new("Invalid table name passed to drop #{table}") if table =~ /[^a-z0-9_]/i
+      validate_table_name(table)
+      columns.each { |column| validate_column_name(column) }
 
-      columns.each do |column|
-        raise ArgumentError.new("Invalid column name passed to drop #{column}") if column =~ /[^a-z0-9_]/i
-      end
-
-      # in production we need some extra delay to allow for slow migrations
-      delay ||= Rails.env.production? ? 3600 : 0
-
-      sql = <<~SQL
-        SELECT 1
-        FROM INFORMATION_SCHEMA.COLUMNS
-        WHERE table_schema = 'public' AND
-          table_name = :table AND
-          column_name IN (:columns) AND
-          EXISTS (
-            SELECT 1
-            FROM schema_migration_details
-            WHERE name = :after_migration AND
-                  created_at <= (current_timestamp at time zone 'UTC' - interval :delay)
-          )
-        LIMIT 1
-      SQL
-
-      if ActiveRecord::Base.exec_sql(sql,
-                                     table: table,
-                                     columns: columns,
-                                     delay: "#{delay.to_i || 0} seconds",
-                                     after_migration: after_migration).to_a.length > 0
-        on_drop&.call
-
-        columns.each do |column|
-          ActiveRecord::Base.exec_sql <<~SQL
-            DROP TRIGGER IF EXISTS #{readonly_trigger_name(table, column)} ON #{table};
-            DROP FUNCTION IF EXISTS #{readonly_function_name(table, column)} CASCADE;
-          SQL
-
-          # safe cause it is protected on method entry, can not be passed in params
-          ActiveRecord::Base.exec_sql("ALTER TABLE #{table} DROP COLUMN IF EXISTS #{column}")
-        end
-
-        Discourse.reset_active_record_cache
-      end
+      ColumnDropper.new(table, columns, after_migration, delay, on_drop).delayed_drop
     end
 
     def self.mark_readonly(table_name, column_name)
-      ActiveRecord::Base.exec_sql <<~SQL
-        CREATE OR REPLACE FUNCTION #{readonly_function_name(table_name, column_name)} RETURNS trigger AS $rcr$
-          BEGIN
-            RAISE EXCEPTION 'Discourse: #{column_name} in #{table_name} is readonly';
-          END
-        $rcr$ LANGUAGE plpgsql;
-      SQL
+      create_readonly_function(table_name, column_name)
 
       ActiveRecord::Base.exec_sql <<~SQL
         CREATE TRIGGER #{readonly_trigger_name(table_name, column_name)}
@@ -67,12 +24,41 @@ module Migration
 
     private
 
-    def self.readonly_function_name(table_name, column_name)
-      "raise_#{table_name}_#{column_name}_readonly()"
+    def initialize(table, columns, after_migration, delay, on_drop)
+      super(after_migration, delay, on_drop)
+
+      @table = table
+      @columns = columns
     end
 
-    def self.readonly_trigger_name(table_name, column_name)
-      "#{table_name}_#{column_name}_readonly"
+    def droppable?
+      builder = SqlBuilder.new(<<~SQL)
+        SELECT 1
+        FROM INFORMATION_SCHEMA.COLUMNS
+        /*where*/
+        LIMIT 1
+      SQL
+
+      builder.where("table_schema = 'public'")
+        .where("table_name = :table")
+        .where("column_name IN (:columns)")
+        .where(previous_migration_done)
+        .exec(table: @table,
+              columns: @columns,
+              delay: "#{@delay} seconds",
+              after_migration: @after_migration).to_a.length > 0
+    end
+
+    def execute_drop!
+      @columns.each do |column|
+        ActiveRecord::Base.exec_sql <<~SQL
+          DROP TRIGGER IF EXISTS #{BaseDropper.readonly_trigger_name(@table, column)} ON #{@table};
+          DROP FUNCTION IF EXISTS #{BaseDropper.readonly_function_name(@table, column)} CASCADE;
+        SQL
+
+        # safe cause it is protected on method entry, can not be passed in params
+        ActiveRecord::Base.exec_sql("ALTER TABLE #{@table} DROP COLUMN IF EXISTS #{column}")
+      end
     end
   end
 end
--- lib/migration/table_dropper.rb
+++ lib/migration/table_dropper.rb
@@ -1,14 +1,16 @@
+require_dependency 'migration/base_dropper'
+
 module Migration
-  class TableDropper
+  class Migration::TableDropper < BaseDropper
+    def self.delayed_drop(old_name:, new_name:, after_migration:, delay: nil, on_drop: nil)
+      validate_table_name(old_name)
+      validate_table_name(new_name)
+
+      TableDropper.new(old_name, new_name, after_migration, delay, on_drop).delayed_drop
+    end
+
     def self.read_only_table(table_name)
-      ActiveRecord::Base.exec_sql <<~SQL
-        CREATE OR REPLACE FUNCTION #{readonly_function_name(table_name)} RETURNS trigger AS $rro$
-          BEGIN
-            RAISE EXCEPTION 'Discourse: Table is read only';
-            RETURN null;
-          END
-        $rro$ LANGUAGE plpgsql;
-      SQL
+      create_readonly_function(table_name)
 
       ActiveRecord::Base.exec_sql <<~SQL
         CREATE TRIGGER #{readonly_trigger_name(table_name)}
@@ -19,51 +21,49 @@ module Migration
       SQL
     end
 
-    def self.delayed_drop(old_name:, new_name:, after_migration:, delay: nil, on_drop: nil)
-      delay ||= Rails.env.production? ? 3600 : 0
+    private
 
-      sql = <<~SQL
+    def initialize(old_name, new_name, after_migration, delay, on_drop)
+      super(after_migration, delay, on_drop)
+
+      @old_name = old_name
+      @new_name = new_name
+    end
+
+    def droppable?
+      builder = SqlBuilder.new(<<~SQL)
         SELECT 1
         FROM INFORMATION_SCHEMA.TABLES
-        WHERE table_schema = 'public' AND
-              EXISTS (
-                SELECT 1
-                FROM schema_migration_details
-                WHERE name = :after_migration AND
-                      created_at <= (current_timestamp at time zone 'UTC' - interval :delay)
-              )
-              AND EXISTS (
-                SELECT 1
-                FROM INFORMATION_SCHEMA.TABLES
-                WHERE table_schema = 'public' AND
-                table_name = :new_name
-              )
+        /*where*/
         LIMIT 1
       SQL
 
-      if ActiveRecord::Base.exec_sql(sql,
-                                     old_name: old_name,
-                                     new_name: new_name,
-                                     delay: "#{delay.to_i || 0} seconds",
-                                     after_migration: after_migration).to_a.length > 0
-
-        on_drop&.call
-        ActiveRecord::Base.exec_sql("DROP TABLE IF EXISTS #{old_name}")
-
-        ActiveRecord::Base.exec_sql <<~SQL
-          DROP FUNCTION IF EXISTS #{readonly_function_name(old_name)} CASCADE;
-        SQL
-      end
+      builder.where("table_schema = 'public'")
+        .where(previous_migration_done)
+        .where(new_table_exists)
+        .exec(old_name: @old_name,
+              new_name: @new_name,
+              delay: "#{@delay} seconds",
+              after_migration: @after_migration).to_a.length > 0
     end
 
-    private
-
-    def self.readonly_function_name(table_name)
-      "public.raise_#{table_name}_read_only()"
+    def new_table_exists
+      <<~SQL
+        EXISTS(
+            SELECT 1
+            FROM INFORMATION_SCHEMA.TABLES
+            WHERE table_schema = 'public' AND
+                  table_name = :new_name
+        )
+      SQL
     end
 
-    def self.readonly_trigger_name(table_name)
-      "#{table_name}_read_only"
+    def execute_drop!
+      ActiveRecord::Base.exec_sql("DROP TABLE IF EXISTS #{@old_name}")
+
+      ActiveRecord::Base.exec_sql <<~SQL
+        DROP FUNCTION IF EXISTS #{BaseDropper.readonly_function_name(@old_name)} CASCADE;
+      SQL
     end
   end
 end
